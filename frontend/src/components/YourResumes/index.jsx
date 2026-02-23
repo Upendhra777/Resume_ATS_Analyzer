bfff427763482078c499fa7ad4dbd60eaa817b19
Upendhra Mutyala <143545224+Upendhra777@users.noreply.github.com>
2026-02-23 18:46:47 +0530

    Update resume upload and analyze URLs to production

--- frontend/src/components/YourResumes/index.jsx
+++ frontend/src/components/YourResumes/index.jsx
@@ -36,7 +36,7 @@ const YourResumes = () => {
  
       // STEP 1️⃣ Upload Resume
       const uploadResponse = await fetch(
-        "http://localhost:5000/resume/upload",
+       "https://backend-resume-ats-analyzer.onrender.com/resume/upload",
         {
           method: "POST",
           headers: {
@@ -61,7 +61,7 @@ const YourResumes = () => {
       };
  
       const analyzeResponse = await fetch(
-        "http://localhost:5000/resume/analyze",
+       "https://backend-resume-ats-analyzer.onrender.com/resume/analyze",
         {
           method: "POST",
           headers: {
