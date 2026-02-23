5f6ff13439b512bcb93fb334e7bbc263e76d6a49
Upendhra Mutyala <143545224+Upendhra777@users.noreply.github.com>
2026-02-23 18:40:32 +0530

    Update login URL to production endpoint

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -22,7 +22,7 @@ const Login = () => {
     event.preventDefault()
     setLoading(true)
     setError("")
-    const url = "http://localhost:5000/auth/login";
+    const url = "https://backend-resume-ats-analyzer.onrender.com/auth/login";
     const userDetails = {
       username,
       password
